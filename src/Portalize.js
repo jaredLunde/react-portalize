0541fc6f8df5a48b56d767d324cab11132c61986
Jared Lunde <jared.lunde@gmail.com>
2019-07-28 12:47:44 -0500

    No longer uses class components, removes babel runtime dependency

--- src/Portalize.js
+++ src/Portalize.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
 
 /**
@@ -9,9 +8,41 @@ import PropTypes from 'prop-types'
 </Portalize>
 */
 export const PORTALS = {}
+const getContainer = container =>
+  typeof document !== 'undefined' && document.querySelectorAll(container)
 
-export default class Portalize extends React.Component {
-  static propTypes = {
+const Portalize = ({container = '#portals', server = true, providers, children}) => {
+  const [nodes, setNodes] = useState(getContainer(container))
+  useEffect(() => {setNodes(getContainer(container))}, [container])
+
+  if (nodes === false) {
+    if (server === false)
+      return null
+
+    if (providers !== void 0 && providers.length > 0) {
+      children = providers.reduceRight(
+        (children, {provider, value}) => React.createElement(provider, {children, value}),
+        children
+      )
+    }
+
+    PORTALS[container] = children
+    return null
+  }
+  else if (nodes.length === 0) {
+    return null
+  }
+  else {
+    let portals = [], i = 0
+    for (; i < nodes.length; i++) portals.push(ReactDOM.createPortal(children, nodes[i]))
+    return portals
+  }
+}
+
+if (__DEV__) {
+  const PropTypes = require('prop-types')
+
+  Portalize.propTypes = {
     container: PropTypes.string.isRequired,
     providers: PropTypes.arrayOf(
       PropTypes.shape({
@@ -21,47 +52,6 @@ export default class Portalize extends React.Component {
     ),
     server: PropTypes.bool.isRequired
   }
-
-  static defaultProps = {
-    container: '#portals',
-    server: true
-  }
-
-  nodes = null
-
-  render () {
-    this.nodes =
-      typeof document !== 'undefined' && document.querySelectorAll(this.props.container)
-
-    if (this.nodes === false) {
-      let {children, server, providers} = this.props
-
-      if (server === false) {
-        return null
-      }
-
-      if (providers !== void 0 && providers.length > 0) {
-        children = providers.reduceRight(
-          (children, {provider, value}) =>
-            React.createElement(provider, {children, value}),
-          children
-        )
-      }
-
-      PORTALS[this.props.container] = children
-      return null
-    }
-    else if (this.nodes.length === 0) {
-      return null
-    }
-    else {
-      const portals = []
-
-      for (let i = 0; i < this.nodes.length; i++) {
-        portals.push(ReactDOM.createPortal(this.props.children, this.nodes[i]))
-      }
-
-      return portals
-    }
-  }
 }
+
+export default Portalize
